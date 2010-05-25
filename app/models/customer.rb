f048bcbf79f924a8d2a86a08b7bf64558511c1e2
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-05-25 12:37:49 +0200

    Now using customers_email_address for authentication to make it fit into the legacy db

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,10 +3,12 @@ class Customer < ActiveRecord::Base
   set_table_name :customers
   set_primary_key :customers_id
 
+  alias_attribute :email, :customers_email_address
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :token_authenticatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :customers_email_address, :password, :password_confirmation
 end
