c16312eec3d2e612b7ad858415b2ad241aab1216
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-05-25 14:20:57 +0200

    Loging in now works with the legacy password field

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,7 @@ class Customer < ActiveRecord::Base
   set_primary_key :customers_id
 
   alias_attribute :email, :customers_email_address
+  alias_attribute :password, :customers_password
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
@@ -11,4 +12,10 @@ class Customer < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :customers_email_address, :password, :password_confirmation
+
+  def valid_password?(provided_password)
+    hash_password, salt = password.split(':')
+    result = Digest::MD5.hexdigest("#{salt}#{provided_password}")
+    return hash_password == result
+  end
 end
