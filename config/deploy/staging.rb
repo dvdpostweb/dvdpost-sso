08381f994b2a9aad60e252f1bffa25ab563f6542
Maxime Robert-Schreyers <mrs@LeFatPro.local>
2010-05-28 12:17:37 +0200

    deployment + removed splite gem from gemfile

---
+++ config/deploy/staging.rb
@@ -0,0 +1,70 @@
+#############################################################
+#	Application
+#############################################################
+
+set :application, "sso"
+set :deploy_to, "/home/webapps/sso/staging"
+
+#############################################################
+#	Settings
+#############################################################
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :use_sudo, false
+set :scm_verbose, true
+set :rails_env, "staging" 
+
+#############################################################
+#	Servers
+#############################################################
+
+set :user, "sso"
+set :domain, "staging.dvdpost.be" # "staging.sso.dvdpost.be"
+set :port, 22012
+server domain, :app, :web
+role :db, domain, :primary => true
+
+#############################################################
+#	Git
+#############################################################
+
+set :scm, :git
+set :branch, "master"
+set :scm_user, 'sso'
+set :scm_passphrase, "[y'|\E7U158]9*"
+set :repository, "git@github.com:redstorm/dvdpost-sso.git"
+set :deploy_via, :remote_cache
+
+#############################################################
+#	Passenger
+#############################################################
+
+namespace :deploy do
+  desc "Create the database yaml file"
+  task :after_update_code do
+    db_config = <<-EOF
+    staging:    
+      adapter: mysql
+      encoding: utf8
+      database: dvdpost_test
+      username: test_devuser
+      password: 1nterD3nt
+      host: www.dvdpost.be
+      port: 43306
+    EOF
+
+    put db_config, "#{release_path}/config/database.yml"
+  end
+  
+  # Restart passenger on deploy
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
