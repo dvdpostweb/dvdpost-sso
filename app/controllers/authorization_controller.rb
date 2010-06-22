16312c418ff0f81583b98983fe6804df053442b4
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-06-22 16:51:57 +0200

    More robust error handling

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -2,21 +2,32 @@ class AuthorizationController < ApplicationController
   before_filter :verify_token, :only => [:me]
 
   def new
-    authenticate_customer!
-    redirect_to callback_url(params[:redirect_uri], {:code => current_customer.generate_verification_code!})
+    if params[:type] == 'web_server'
+      if params[:client_id] == 'dvdpost_client'
+        if params[:redirect_uri]
+          authenticate_customer!
+          redirect_to callback_url(params[:redirect_uri], {:code => current_customer.generate_verification_code!})
+        else
+          render_bad_request :redirect_uri_mismatch
+        end
+      else
+        render_bad_request :invalid_client_credentials
+      end
+    else
+      render_bad_request :invalid_client_type
+    end
   end
 
   def token
     if params[:client_id] == 'dvdpost_client'
-      # params[:client_secret] == 'dvdpost_client_secret' # => not required
-      # params[:redirect_uri] == 'some_uri'               # => not required (error code = redirect_uri_mismatch)
+      # params[:client_secret] == 'some_secret' # Clients should be reigstered and their secret should be validated too
       case params[:grant_type]
         when 'authorization_code' then authorization_code(params)
         when 'refresh_token'      then refresh_token(params)
-        else render_bad_request 'unsupported_grant_type'
+        else render_bad_request :unsupported_grant_type
       end
     else
-      render_bad_request 'client_id_mismatch'
+      render_bad_request :invalid_client_credentials
     end
   end
 
@@ -32,38 +43,41 @@ class AuthorizationController < ApplicationController
 
   private
   def authorization_code(params)
-    customer = Customer.find_by_verification_code(params[:code])
-    generate_and_return_tokens(customer)
+    if params[:redirect_uri] # More secure would be to check if it's a registered redirect_uri
+      customer = Customer.find_by_verification_code(params[:code])
+      generate_and_return_tokens customer, :invalid_authorization_code
+    else
+      render_bad_request :redirect_uri_mismatch
+    end
   end
 
   def refresh_token(params)
     customer = Customer.find_by_refresh_token(params[:refresh_token])
-    generate_and_return_tokens(customer)
+    generate_and_return_tokens customer, :invalid_refresh_token
   end
 
   def verify_token
-    # Actually we could have used authenticate_customer! in here since it can validate by the access_token
-    # But that would redirect us to the login page which is against the specifications of OAuth2
-    # This actually should be oauth_token
-    oauth_token = params[:access_token]
+    oauth_token = params[:oauth_token] || params[:access_token] # Current client gem does not support oauth_token yet
     if oauth_token
-      unless current_customer == Customer.find_by_authentication_token(oauth_token)
-        render_unauthorized
+      if current_customer == Customer.find_by_authentication_token(oauth_token)
+        render_unauthorized :authroziation_expired if current_customer.access_token_expired?
+      else
+        render_unauthorized :invalid_access_token
       end
     else
-      render_unauthorized
+      render_unauthorized :invalid_access_token
     end
   end
 
-  def generate_and_return_tokens(customer)
+  def generate_and_return_tokens(customer, error_message)
     if customer && customer.update_tokens!
       render :status => :ok, :json => {:access_token => customer.authentication_token, :expires_in => customer.access_token_expires_in, :refresh_token => customer.refresh_token}
     else
-      render_bad_request 'There was a problem trying to retrieve the token.'
+      render_bad_request error_message
     end
   end
 
-  def render_unauthorized(message='invalid_token')
+  def render_unauthorized(message)
     warden.custom_failure!
     render_error :unauthorized, message
   end
