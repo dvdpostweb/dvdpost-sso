37085b1ff96e424d8ce815b0669eba750d6cccdb
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-06-22 17:12:04 +0200

    Displaying errors in the logs

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -88,6 +88,7 @@ class AuthorizationController < ApplicationController
   end
 
   def render_error(status, message)
+    logger.error "*** Error: #{status} => #{message} ***"
     render :status => status, :json => {:error => message}
   end
 end
