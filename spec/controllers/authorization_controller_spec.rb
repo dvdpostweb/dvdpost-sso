99568ac48f8759bab9f71ef956eeb5a32a15577c
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-06-23 14:42:33 +0200

    Implemented specs for authroziation code and token generation

--- spec/controllers/authorization_controller_spec.rb
+++ spec/controllers/authorization_controller_spec.rb
@@ -7,69 +7,130 @@ describe AuthorizationController, "Authorization" do
   include Devise::TestHelpers
 
   before(:each) do
-    @client ||= OAuth2::Client.new( 'dvdpost_client',
-                                    'dvdpost_client_secret',
-                                    :site => SITE_URL,
-                                    :authorize_path => 'authorization/new',
-                                    :access_token_path => 'authorization/token')
     @type ||= 'web_server'
     @client_id ||= 'dvdpost_client'
     @redirect_uri ||= 'http://dvdpost.dev/callback'
+
+    @invalid_type ||= 'invalid_type'
+    @invalid_client_id ||= 'invalid_client_id'
+    @invalid_redirect_uri ||= 'http://my_hack_site.dev/callback'
+    @malformed_redirect_uri ||= 'malformed://dvdpost.dev/callback'
+
     @valid_start_params ||= {:type => @type, :client_id => @client_id, :redirect_uri => @redirect_uri}
+    @valid_authorization_code_params ||= {:client_id => @client_id, :grant_type => 'authorization_code', :redirect_uri => @redirect_uri}
   end
 
-  context "Client requests authorization" do
+  context "new" do
     it "should throw an error 'invalid_client_type' if no type param was given" do
       get :new, :client_id => @client_id, :redirect_uri => @redirect_uri
       response.should be_bad_request
       JSON.parse(response.body)['error'].should == 'invalid_client_type'
     end
 
-    it "should throw an error 'invalid_client_credentials' if no client_id param was given" do
+    it "should throw an error 'invalid_client_type' if an invalid type param was given" do
+      get :new, :type => @invalid_type, :client_id => @client_id, :redirect_uri => @redirect_uri
+      response.should be_bad_request
+      JSON.parse(response.body)['error'].should == 'invalid_client_type'
+    end
+
+    it "should throw an error 'invalid_client_credentials' if no client_id was given" do
       get :new, :type => @type, :redirect_uri => @redirect_uri
       response.should be_bad_request
       JSON.parse(response.body)['error'].should == 'invalid_client_credentials'
     end
 
-    it "should throw an error 'redirect_uri_mismatch' if no redirect_uri param was given" do
+    it "should throw an error 'invalid_client_credentials' if an invalid client_id was given" do
+      get :new, :client_id => @invalid_client_id, :type => @type, :redirect_uri => @redirect_uri
+      response.should be_bad_request
+      JSON.parse(response.body)['error'].should == 'invalid_client_credentials'
+    end
+
+    it "should throw an error 'redirect_uri_mismatch' if no redirect_uri was given" do
       get :new, :type => @type, :client_id => @client_id
       response.should be_bad_request
       JSON.parse(response.body)['error'].should == 'redirect_uri_mismatch'
     end
 
-    it "should redirect to login when not authenticated" do
-      # get :new, @valid_start_params
-      # puts response.body
-      # response.should redirect_to(new_customer_session_path)
+    it "should throw an e rror 'redirect_uri_mismatch' if an incorrect redirect_uri was given" do
+      pending "This is not yet validated in the code"
+      get :new, :type => @type, :client_id => @client_id, :redirect_uri => @invalid_redirect_uri
     end
 
-    it "should return a token when authenticated" do
-      # get :new, @valid_start_params
-      # response.should redirect_to(@redirect_uri)
+
+    it "should throw an e rror 'redirect_uri_mismatch' if a malformed redirect_uri was given" do
+      pending "This is not yet validated in the code"
+      get :new, :type => @type, :client_id => @client_id, :redirect_uri => @malformed_redirect_uri
     end
 
-    it "should return error=user_denied if user denied"
+    it "should redirect to login when the customer is not authenticated" do
+      pending "Fails because of an error: \"undefined method `generate_verification_code!' for nil:NilClass\""
+      get :new, @valid_start_params
+      response.should redirect_to(new_customer_session_path)
+    end
 
-    it "should redirect to the callback with a verification code in the params"
+    it "should redirect to redirect_uri with an authorization code when the customer is authenticated" do
+      pending "Fails because of an error: \"undefined method `generate_verification_code!' for nil:NilClass\""
+      get :new, @valid_start_params
+      response.should redirect_to(@redirect_uri)
+    end
   end
 
-  context "Client Requests Access Token" do
-    it "should return an access token with valid params"
+  context "generate a new token" do
+    it "should throw an error 'invalid_client_credentials' if no client_id was given" do
+      post :token
+      response.should be_bad_request
+      JSON.parse(response.body)['error'].should == 'invalid_client_credentials'
+    end
 
-    it "should return an error redirect_uri_mismatch when a wrong redirect_uri is given"
-
-    it "should return an error bad_verification_code when a wrong verification_code is given"
-
-    it "should return an error incorrect_client_credentials when a wrong client_id is given"
+    it "should throw an error 'unsupported_grant_type' if no grant_type was given" do
+      post :token, :client_id => @client_id
+      response.should be_bad_request
+      JSON.parse(response.body)['error'].should == 'unsupported_grant_type'
+    end
   end
 
-  context "Client Refreshes Access Token" do
-    it "should return a new access_token when valid params"
+  context "generate a new token with an authorization code" do
+    it "should throw an error 'redirect_uri_mismatch' if no redirect_uri was given" do
+      post :token, :client_id => @client_id, :grant_type => 'authorization_code'
+      response.should be_bad_request
+      JSON.parse(response.body)['error'].should == 'redirect_uri_mismatch'
+    end
 
-    it "should return an error incorrect_client_credientials when a wrong client_id is given"
+    it "should thrown an error 'redirect_uri_mismatch' if an incorrect redirect_uri was given" do
+      pending "Validation on redirect_uri's is not implemented."
+    end
 
-    it "should return an error authorization_expired when authorization is expired"
+    it "should thrown an error 'invalid_authorization_code' if an incorrect authorization_code was given" do
+      pending "Fails because this requires access to the database we cannot provide because of this legacy database"
+      post :token, @valid_authorization_code_params.merge(:authorization_code => 'invalid_authorization_code')
+      response.should be_bad_request
+      JSON.parse(response.body)['error'].should == 'invalid_authorization_code'
+    end
 
-    it "should return an error unsupported_secret_type when a wrong secret_type is given"
+    it "should return an access token, refresh token and expiry date if a correct authorization_code was given" do
+      pending "Fails because this requires access to the database we cannot provide because of this legacy database"
+      post :token, @valid_authorization_code_params.merge(:authorization_code => 'valid_authorization_code')
+    end
+  end
+
+  context "generate a new token with a refresh token" do
+    it "should thrown an error 'invalid_refresh_token' if an incorrect refresh_token was given" do
+      pending "Fails because this requires access to the database we cannot provide because of this legacy database"
+      post :token, @valid_authorization_code_params.merge(:refresh_token => 'invalid_refresh_token')
+      response.should be_bad_request
+      JSON.parse(response.body)['error'].should == 'invalid_refresh_token'
+    end
+
+    it "should thrown an error 'invalid_refresh_token' if an expired refresh_token was given" do
+      pending "Fails because this requires access to the database we cannot provide because of this legacy database"
+      post :token, @valid_authorization_code_params.merge(:refresh_token => 'expired_refresh_token')
+      response.should be_bad_request
+      JSON.parse(response.body)['error'].should == 'invalid_refresh_token'
+    end
+
+    it "should return an access token, refresh token and expiry date if a correct refresh_token was given" do
+      pending "Fails because this requires access to the database we cannot provide because of this legacy database"
+      post :token, @valid_authorization_code_params.merge(:refresh_token => 'valid_refresh_token')
+    end
   end
 end
