49edad3378513da7db224f9ded52f1aadf4c0795
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-06-24 15:03:36 +0200

    Revert "Bundler enhancements in deploy script"
    This reverts commit 27cd960a6f46c6f63f6a615a7f8ccd63d1066719.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,17 +2,10 @@ set :stages, %w(staging production)
 set :default_stage, 'staging'
 require 'capistrano/ext/multistage'
 
-namespace :bundler do
-  task :create_symlink, :roles => :app do
-    shared_dir = File.join(shared_path, 'bundle')
-    release_dir = File.join(current_release, '.bundle')
-    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
-  end
-
-  task :bundle_new_release, :roles => :app do
-    bundler.create_symlink
-    run "cd #{release_path} ; export PATH=/opt/ruby/bin:$PATH ; bundle check 2>&1 > /dev/null ; if [ $? -ne 0 ] ; then sh -c 'bundle install --disable-shared-gems --without test' ; fi"
+namespace :deploy do
+  task :bundle_gems do
+    run "cd #{current_path} && export PATH=/opt/ruby/bin:$PATH && bundle install --relock"
   end
 end
 
-after "deploy:symlink", "bundler:bundle_new_release"
+after 'deploy:symlink', 'deploy:bundle_gems'
