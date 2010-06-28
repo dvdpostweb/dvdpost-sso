129062103484a9380e57431b2fe049fd889f8a12
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-06-28 13:59:12 +0200

    Removed unused /login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 DvdpostSso::Application.routes.draw do |map|
   devise_for :customers
-  get 'login',  :to => redirect('/customers/sign_in')
 
   controller :authorization do
     namespace :authorization do
