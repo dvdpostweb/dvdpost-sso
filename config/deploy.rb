d242ab882c3b73118b209777c62caa3a65365e2a
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-07-07 10:54:40 +0200

    Updated bundler deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,10 +2,17 @@ set :stages, %w(staging production)
 set :default_stage, 'staging'
 require 'capistrano/ext/multistage'
 
-namespace :deploy do
-  task :bundle_gems do
-    run "cd #{current_path} && export PATH=/opt/ruby/bin:$PATH && bundle install --without test --relock"
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(current_release, '.bundle')
+    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+  end
+
+  task :bundle_new_release, :roles => :app do
+    bundler.create_symlink
+    run "cd #{release_path} ; export PATH=/opt/ruby/bin:$PATH ; bundle check 2>&1 > /dev/null ; if [ $? -ne 0 ] ; then sh -c 'bundle install --relock --disable-shared-gems --without test' ; fi"
   end
 end
 
-after 'deploy:symlink', 'deploy:bundle_gems'
+after 'deploy:symlink', 'bundler:bundle_new_release'
