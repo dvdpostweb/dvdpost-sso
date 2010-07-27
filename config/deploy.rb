bd3ee9b33211e85fcc428d6176110c6e5148dc89
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-07-27 14:36:21 +0200

    Enhanced deploy script for bundler

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,9 +4,16 @@ require 'capistrano/ext/multistage'
 require 'hoptoad_notifier/capistrano'
 
 namespace :bundler do
-  task :install, :roles => :app do
-    run "cd #{release_path}; export PATH=/opt/ruby/bin:$PATH; bundle install vendor --disable-shared-gems --without test"
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(current_release, 'vendor', 'bundle')
+    run "mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}"
+  end
+
+  task :bundle_new_release, :roles => :app do
+    bundler.create_symlink
+    run "cd #{release_path}; export PATH=/opt/ruby/bin:$PATH; bundle install vendor/bundle --disable-shared-gems --without test"
   end
 end
 
-after 'deploy:symlink', 'bundler:install'
+after "deploy:symlink", "bundler:bundle_new_release"
