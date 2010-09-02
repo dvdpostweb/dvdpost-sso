16037f4f72b2d47a360e1e658485e77461049a6e
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-09-02 16:51:12 +0200

    Updated to Bundler 1.0.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,18 +2,4 @@ set :stages, %w(staging production)
 set :default_stage, 'staging'
 require 'capistrano/ext/multistage'
 require 'hoptoad_notifier/capistrano'
-
-namespace :bundler do
-  task :create_symlink, :roles => :app do
-    shared_dir = File.join(shared_path, 'bundle')
-    release_dir = File.join(current_release, 'vendor', 'bundle')
-    run "mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}"
-  end
-
-  task :bundle_new_release, :roles => :app do
-    bundler.create_symlink
-    run "cd #{release_path}; export PATH=/opt/ruby/bin:$PATH; bundle install --production --without test"
-  end
-end
-
-after "deploy:symlink", "bundler:bundle_new_release"
+require 'bundler/capistrano'
