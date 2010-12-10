e8e50f02536dacb3eb9ca307cc965b862c843ed4
guillaume <gs@dvdpost.be>
2010-12-10 11:37:26 +0100

    change secret cookie

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = 'cf362989419f7444f1a519d52863423828d866cd4404ae29e9d0c142157570b00e4e411e6cc9105b211439e9a957dbf8108fedf6ffb7ec01cc71891aabbba8cd'
+Rails.application.config.secret_token = 'cf362989419f7444f1a519d52863423828d866cd4404ae29e9d0c142157570b00e4e411e6cc9105b211439e9a957dbf8108fedf6ffb7ec01cc71891aabbba8cda'
