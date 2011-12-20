b7e842950db99aef8fe58ad2e6f3a0ff9e64daa2
guillaume <gs@dvdpost.be>
2011-12-20 16:34:57 +0100

    iphone app

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -34,7 +34,7 @@ set :branch, "master"
 set :scm_user, 'sso'
 set :scm_passphrase, "[y'|\E7U158]9*"
 set :repository, "git@github.com:dvdpost/dvdpost-sso.git"
-set :deploy_via, :remote_cache
+#set :deploy_via, :remote_cache
 
 #############################################################
 #	Passenger
