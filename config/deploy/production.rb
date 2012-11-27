2260cf3947b286f36b439bf7b762114d134f1678
guillaume <gs@dvdpost.be>
2012-11-27 10:55:48 +0100

    deploy in both server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -23,8 +23,8 @@ set :user, "sso"
 set :domain,  "217.112.190.177"
 set :domain2, "94.139.62.122"
 set :port, 22012
-role :web, domain2
-role :app, domain2
+role :web, domain, domain2
+role :app, domain, domain2
 role :db, domain2, :primary => true
 
 #############################################################
