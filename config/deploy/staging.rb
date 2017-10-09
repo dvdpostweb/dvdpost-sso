dc3075a3ea5a04ebb9e10ac27bd4db6abafedd46
YOUR NAME <YOUR@EMAIL.com>
2017-10-09 11:25:50 +0200

    set db for staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -13,7 +13,7 @@ default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 set :use_sudo, false
 set :scm_verbose, true
-set :rails_env, "staging" 
+set :rails_env, "staging"
 
 #############################################################
 #	Servers
@@ -46,25 +46,25 @@ namespace :deploy do
   desc "Create the database yaml file"
   after "deploy:update_code" do
     db_config = <<-EOF
-    staging:    
+    staging:
       adapter: mysql
       encoding: utf8
-      database: dvdpost_test
-      username: test_devuser
-      password: 1nterD3nt
+      database: dvdpost_be_prod
+      username: webuser
+      password: 3gallfir-
       host: 192.168.100.204
       port: 3306
     EOF
 
     put db_config, "#{release_path}/config/database.yml"
   end
-  
+
   # Restart passenger on deploy
   desc "Restarting mod_rails with restart.txt"
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{current_path}/tmp/restart.txt"
   end
-  
+
   [:start, :stop].each do |t|
     desc "#{t} task is a no-op with mod_rails"
     task t, :roles => :app do ; end
